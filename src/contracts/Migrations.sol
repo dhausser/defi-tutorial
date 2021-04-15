415472b487880de106f5862c32af9b7fb712d0ca
Davy Hausser <davy.hausser@gmail.com>
2021-04-15 21:57:15 +0200

    contracts pragma update

--- src/contracts/Migrations.sol
+++ src/contracts/Migrations.sol
@@ -1,18 +1,18 @@
-pragma solidity ^0.5.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 contract Migrations {
     address public owner;
-    uint public last_completed_migration;
+    uint256 public last_completed_migration;
 
     constructor() public {
-        owner = msg.sender; 
+        owner = msg.sender;
     }
 
     modifier restricted() {
         if (msg.sender == owner) _;
     }
 
-    function setCompleted(uint completed) public restricted {
+    function setCompleted(uint256 completed) public restricted {
         last_completed_migration = completed;
     }
 
