cd6c74bf884241a3f610074d143c461a60e05a03
Davy Hausser <davy.hausser@gmail.com>
2021-04-15 19:22:29 +0200

    contracts deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,24 @@
+/* eslint-disable no-undef */
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm)
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock DAI Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DaiToken.deployed()
+
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = TokenFarm.deployed()
+
+  // Transfer all token to TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  // Transfer 100 Mock DAI to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000')
 }
