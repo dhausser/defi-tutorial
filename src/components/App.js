2e7c7f4fa07581565f02d65ebf607c4874201340
Davy Hausser <davy.hausser@gmail.com>
2021-04-15 17:42:40 +0200

    functional component refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navbar from './Navbar'
 import './App.css'
 
-class App extends Component {
+function App() {
+  const [account] = React.useState('0x0')
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      account: '0x0'
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar account={this.state.account} />
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                </a>
-
-                <h1>Hello, World!</h1>
-
-              </div>
-            </main>
-          </div>
+  return (
+    <div>
+      <Navbar account={account} />
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <main
+            role="main"
+            className="col-lg-12 ml-auto mr-auto"
+            style={{ maxWidth: '600px' }}
+          >
+            <div className="content mr-auto ml-auto">
+              <h1>Hello, World!</h1>
+            </div>
+          </main>
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
-export default App;
+export default App
