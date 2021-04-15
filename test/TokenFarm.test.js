d7b250b2ef64a295b0df13afb27936f930125071
Davy Hausser <davy.hausser@gmail.com>
2021-04-15 19:59:52 +0200

    contracts test

---
+++ test/TokenFarm.test.js
@@ -0,0 +1,20 @@
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
+const TokenFarm = artifacts.require('TokenFarm')
+
+require('chai')
+  .use(require('chai-as-promised')
+  .should()
+
+contract('TokenFarm', (accounts) => {
+
+  // Write tests here...
+  describe('Mock Dai deployement', async () => {
+    it('has a name', async () => {
+      let daiToken = await DaiToken.new()
+      const name = await daiToken.name()
+      assert.equal(name, 'Mock Dai Token')
+    })
+  })
+
+})
