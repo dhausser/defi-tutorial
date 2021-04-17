8dc1e5288d0fda949c8aa9a18c13911f0c8549c5
Davy Hausser <davy.hausser@gmail.com>
2021-04-17 09:11:07 +0200

    contracts format 2 spaces

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -4,12 +4,12 @@ import "./DappToken.sol";
 import "./DaiToken.sol";
 
 contract TokenFarm {
-    string public name = "Dapp Token Farm";
-    DappToken public dappToken;
-    DaiToken public daiToken;
+  string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
 
-    constructor(DappToken _dappToken, DaiToken _daiToken) public {
-        dappToken = _dappToken;
-        daiToken = _daiToken;
-    }
+  constructor(DappToken _dappToken, DaiToken _daiToken) public {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
+  }
 }
