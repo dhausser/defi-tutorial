3d46270ebd92d0ee0bc6d901e8808d8d32b37383
Davy Hausser <davy.hausser@gmail.com>
2021-04-17 09:22:50 +0200

    test for tokens and token farm

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -7,12 +7,46 @@ require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('TokenFarm', accounts => {
+function tokens(n) {
+  return web3.utils.toWei(n, 'ether')
+}
+
+contract('TokenFarm', ([owner, investor]) => {
+  let daiToken, dappToken, tokenFarm
+
+  before(async () => {
+    // Load Contracts
+    daiToken = await DaiToken.new()
+    dappToken = await DappToken.new()
+    tokenFarm = await TokenFarm.new(dappToken.address, daiToken.address)
+
+    // Transfer all Dapp tokens to farm (1 million)
+    await dappToken.transfer(tokenFarm.address, tokens('1000000'))
+
+    // Send tokens to investor
+    await daiToken.transfer(investor, tokens('100'), { from: owner })
+  })
+
   describe('Mock Dai deployement', async () => {
     it('has a name', async () => {
       let daiToken = await DaiToken.new()
       const name = await daiToken.name()
       assert.equal(name, 'Mock DAI Token')
+    })
+  })
+
+  describe('Dapp Token deployement', async () => {
+    it('has a name', async () => {
+      let dappToken = await DappToken.new()
+      const name = await dappToken.name()
+      assert.equal(name, 'DApp Token')
+    })
+  })
+
+  describe('Token Farm deployement', async () => {
+    it('has a name', async () => {
+      const name = await tokenFarm.name()
+      assert.equal(name, 'Dapp Token Farm')
     })
   })
 })
