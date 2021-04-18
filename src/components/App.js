72c0d7b1f677b0eb27519f92117036a2afaf549b
Davy Hausser <davy.hausser@gmail.com>
2021-04-18 14:30:18 +0200

    loadWeb3 and loadBlockChainData complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,97 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Web3 from 'web3'
+import DaiToken from '../abis/DaiToken.json'
+import DappToken from '../abis/DappToken.json'
+import TokenFarm from '../abis/TokenFarm.json'
 import './App.css'
 
+async function loadBlockChainData(setState) {
+  const web3 = window.web3
+
+  const accounts = await web3.eth.getAccounts()
+  const account = accounts[0]
+
+  const networkId = await web3.eth.net.getId()
+
+  // // Load DaiToken
+  const daiTokenData = DaiToken.networks[networkId]
+  let daiToken, daiTokenBalance
+  if(daiTokenData) {
+    daiToken = new web3.eth.Contract(DaiToken.abi, daiTokenData.address)
+    daiTokenBalance = await daiToken.methods.balanceOf(account).call()
+    daiTokenBalance = daiTokenBalance.toString()
+  } else {
+    window.alert('DaiToken contract not deployed to detected network.')
+  }
+
+  // Load DappToken
+  const dappTokenData = DaiToken.networks[networkId]
+  let dappToken, dappTokenBalance
+  if(dappTokenData) {
+    dappToken = new web3.eth.Contract(DappToken.abi, dappTokenData.address)
+    dappTokenBalance = await dappToken.methods.balanceOf(account).call()
+    dappTokenBalance = dappTokenBalance.toString()
+  } else {
+    window.alert('DaiToken contract not deployed to detected network.')
+  }
+
+  // Load TokenFarm
+  const tokenFarmData = TokenFarm.networks[networkId]
+  let tokenFarm, stakingBalance
+  if(tokenFarmData) {
+    tokenFarm = new web3.eth.Contract(TokenFarm.abi, tokenFarmData.address)
+    stakingBalance = await tokenFarm.methods.stakingBalance(account).call()
+    stakingBalance = stakingBalance.toString()
+  } else {
+    window.alert('TokenFarm contract not deployed to detected network.')
+  }
+ 
+  setState({
+    account,
+    daiToken,
+    daiTokenBalance,
+    dappToken,
+    dappTokenBalance,
+    tokenFarm, 
+    stakingBalance,
+    loading: false
+  })
+}
+
+async function loadWeb3() {
+  if (window.ethereum) {
+    window.web3 = new Web3(window.ethereum)
+    await window.ethereum.enable()
+  }
+  else if (window.web3) {
+    window.web3 = new Web3(window.web3.currentProvider)
+  }
+  else {
+    window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+  }
+}
+
 function App() {
-  const [account] = React.useState('0x0')
+  const [state, setState] = React.useState({
+    account: '0x0',
+    daiToken: null,
+    daiTokenBalance: 0,
+    dappToken: null,
+    dappTokenBalance: 0,
+    tokenFarm: null,
+    stakingBalance: 0,
+    loading: true
+  })
+
+  React.useEffect(() => {
+    loadWeb3()
+    loadBlockChainData(setState)
+  }, [])
 
   return (
     <div>
-      <Navbar account={account} />
+      <Navbar account={state.account} />
       <div className="container-fluid mt-5">
         <div className="row">
           <main
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import farmer from '../farmer.png'
 
-function Navbar() {
+function Navbar({ account }) {
   return (
     <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-      <a
-        className="navbar-brand col-sm-3 col-md-2 mr-0"
-        href="http://www.dappuniversity.com/bootcamp"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
+      <div className="navbar-brand col-sm-3 col-md-2 mr-0">
         <img
           src={farmer}
           width="30"
@@ -18,12 +13,12 @@ function Navbar() {
           alt=""
         />
         &nbsp; DApp Token Farm
-      </a>
+      </div>
 
       <ul className="navbar-nav px-3">
         <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
           <small className="text-secondary">
-            <small id="account">{this.props.account}</small>
+            <small id="account">{account}</small>
           </small>
         </li>
       </ul>
