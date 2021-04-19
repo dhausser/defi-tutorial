abc6880dc4b0a9ce7f58029da46f930cddd562d8
Davy Hausser <davy.hausser@gmail.com>
2021-04-19 22:16:08 +0200

    dapp and staking balances

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,25 @@
 import React from 'react'
+import dai from '../dai.png'
 
-function Main({
-  daiTokenBalance,
-  dappTokenBalance,
-  stakingBalance,
-  stakeTokens,
-  unstakeTokens
-}) {
-  return <h1>Hello, World!</h1>
+function Main({ daiTokenBalance, dappTokenBalance, stakingBalance, stakeTokens, unstakeTokens }) {
+  return (
+    <div id="content" className="mt-3">
+      <table className="table table-borderless text-muted text-center">
+        <thead>
+          <tr>
+            <th scope="col">Staking Balance</th>
+            <th scope="col">Reward Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{window.web3.utils.fromWei(stakingBalance, 'Ether')} mDAI</td>
+            <td>{window.web3.utils.fromWei(dappTokenBalance, 'Ether')} DAPP</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
 export default Main
